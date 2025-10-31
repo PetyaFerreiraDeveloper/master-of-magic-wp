420449420c2f3b828e6d9a2d413cc717063fb8a4
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2025-10-31 11:57:53 +0100

    add page.html

---
+++ wp-content/themes/masterofmagic/patterns/template-page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Page
+ * Slug: masterofmagic-theme/template-page
+ * Description: The page that shows when a general page is created. No title is displayed.
+ * Categories: masterofmagic-theme/pages
+ * Keywords: page, full-width
+ *
+ * @formatter Prettier
+ * Inserter: false
+ *
+ * @package masterofmagic-theme
+ */
+
+?>
+
+<!-- wp:post-content {"align":"full","layout":{"type":"constrained"}} /-->

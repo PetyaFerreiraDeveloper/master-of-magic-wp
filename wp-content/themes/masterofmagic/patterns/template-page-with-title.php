5bfbbe23ff7d049aedbf0426276587bcef5eda77
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2025-10-31 12:49:42 +0100

    create page with title html and pattern

---
+++ wp-content/themes/masterofmagic/patterns/template-page-with-title.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Page
+ * Slug: masterofmagic-theme/template-page-with-title
+ * Description: The page that shows when a general page with title is created.
+ * Categories: masterofmagic-theme/pages
+ * Keywords: page, full-width
+ *
+ * @formatter Prettier
+ * Inserter: false
+ *
+ * @package masterofmagic-theme
+ */
+
+?>
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+	<!-- wp:post-title {"level":1,"fontSize":"3-xl"} /-->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:post-content {"align":"full","layout":{"type":"constrained"}} /-->

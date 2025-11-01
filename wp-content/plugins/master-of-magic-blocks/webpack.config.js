5403d622d5b218678e7914ccd696d2a2020bde8b
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2025-11-01 12:09:23 +0100

    add webpack and package.json to plugin

---
+++ wp-content/plugins/master-of-magic-blocks/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+
+/**
+ * When hasExperimentalModulesFlag is enabled, the defaultConfig is an array of objects.
+ * Loop through both sets of config objects and add the CopyWebpackPlugin to each.
+ */
+const newDefaultConfig = defaultConfig.map((config) => {
+    config.plugins = [
+        ...config.plugins,
+        // Copy SVG files from src to build.
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: '**/*.svg',
+                    context: path.resolve(__dirname, 'src'),
+                    to: path.resolve(__dirname, 'build'),
+                },
+            ],
+        }),
+    ];
+    return config;
+});
+
+module.exports = newDefaultConfig;

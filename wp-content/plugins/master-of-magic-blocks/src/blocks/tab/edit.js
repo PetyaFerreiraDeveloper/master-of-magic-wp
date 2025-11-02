5f7e8a98e51d820eb5430c73d3dfe4acb9612ee0
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2025-11-02 09:58:34 +0100

    create a tab block

---
+++ wp-content/plugins/master-of-magic-blocks/src/blocks/tab/edit.js
@@ -0,0 +1,9 @@
+/**
+ * WordPress dependencies.
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+
+export default function Edit() {
+  const blockProps = useBlockProps({ className: 'master-of-magic-blocks-tab' });
+  return <div {...blockProps}>Hello from the editor</div>;
+}
---
+++ wp-content/plugins/master-of-magic-blocks/src/blocks/tab/render.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * PHP file to use when rendering the block type on the server to show on the front end.
+ *
+ * The following variables are exposed to the file:
+ *     $attributes (array): The block attributes.
+ *     $block (WP_Block): The block instance.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ * @formatter Prettier
+ * @package master-of-magic-blocks
+ */
+
+?>
+
+<div class="master-of-magic-blocks-tab">
+	<?php
+	echo esc_html__( 'Hello from the front end', 'master-of-magic-blocks' );
+	?>
+</div>

83810b68c18c3e51fc721514d825922a4fbb5d5b
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2025-11-14 14:17:32 +0100

    make the label of the tab editable

--- wp-content/plugins/master-of-magic-blocks/src/blocks/tabs/edit.js
+++ wp-content/plugins/master-of-magic-blocks/src/blocks/tabs/edit.js
@@ -2,16 +2,20 @@
  * Wordpress dependencies
  */
 
+import { __ } from '@wordpress/i18n';
 import { useBlockProps, InnerBlocks, RichText } from '@wordpress/block-editor';
-import { withSelect } from '@wordpress/data';
+import { withSelect, withDispatch } from '@wordpress/data';
 import { compose } from '@wordpress/compose';
 
 /**
  * Edit component for Tabs block
  */
-function Edit({ innerBlocks }) {
-  console.log('Inner blocks:', innerBlocks);
+function Edit({ innerBlocks = [], setInnerBlockAttributes }) {
   const blockProps = useBlockProps();
+
+  const navItemClassName =
+    'wp-block-abtion-block-library-tabs__navigation-item';
+
   return (
     <div {...blockProps}>
       <div className="wp-block-master-of-magic-blocks-tabs__navigation">
@@ -21,6 +25,14 @@ function Edit({ innerBlocks }) {
               key={index}
               tagName="div"
               value={block.attributes.label}
+              placeholder={__('Tab label', 'master-of-magic-blocks')}
+              allowedFormats={[]} // no bold/italic etc, just plain text
+              className={navItemClassName}
+              onChange={label =>
+                setInnerBlockAttributes(block.clientId, {
+                  attributes: { label },
+                })
+              }
             />
           ))}
         </div>
@@ -33,5 +45,10 @@ function Edit({ innerBlocks }) {
 export default compose([
   withSelect((select, props) => ({
     innerBlocks: select('core/block-editor').getBlocks(props.clientId),
+  })),
+  withDispatch(dispatch => ({
+    setInnerBlockAttributes: (clientId, attributes) => {
+      dispatch('core/block-editor').updateBlock(clientId, attributes);
+    },
   })),
 ])(Edit);

03b9e6c40dcd59e8655fa469b57f63eeeaa5f017
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2026-01-16 15:11:43 +0100

    add an image hero pattern

---
+++ wp-content/themes/master-of-magic-theme/patterns/image-hero.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: Image Hero
+ * Slug: master-of-magic-theme/image-hero
+ * Categories: heroes
+ * Description: Image hero pattern for Master of Magic Theme.
+ *
+ * @package master-of-magic-theme
+ * @formatter Prettier
+ */
+
+?>
+
+<!-- wp:cover {"dimRatio":0,"isUserOverlayColor":true,"minHeight":70,"minHeightUnit":"vh","sizeSlug":"large","metadata":{"name":"Hero Image"},"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull" style="min-height:70vh">
+	<span
+	aria-hidden="true"
+	class="wp-block-cover__background has-background-dim-0 has-background-dim"
+	></span>
+	<div class="wp-block-cover__inner-container"></div>
+</div>
+<!-- /wp:cover -->

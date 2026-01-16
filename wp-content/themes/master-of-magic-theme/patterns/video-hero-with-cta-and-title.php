19f4d8effb428e98718e1b2e3e4db2c9d6a8de44
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2026-01-16 14:49:45 +0100

    create two hero patterns

---
+++ wp-content/themes/master-of-magic-theme/patterns/video-hero-with-cta-and-title.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Title: Video Hero With CTA and Title
+ * Slug: master-of-magic-theme/video-hero-with-cta-and-title
+ * Categories: heroes
+ * Description: Video hero with cta and title pattern for Master of Magic Theme.
+ *
+ * @package master-of-magic-theme
+ * @formatter Prettier
+ */
+
+?>
+
+<!-- wp:cover {"dimRatio":30,"overlayColor":"off-black-950","isUserOverlayColor":true,"backgroundType":"video","minHeight":70,"minHeightUnit":"vh","isDark":false,"sizeSlug":"full","metadata":{"categories":["hero"],"name":"Video Hero with CTA and Title","patternName":"master-of-magic-theme/video-hero-with-title"},"align":"full","style":{"spacing":{"padding":{"top":"5vw","right":"5vw","bottom":"5vw","left":"5vw"},"margin":{"top":"0"}}}} -->
+<div
+	class="wp-block-cover alignfull is-light"
+	style="margin-top:0;padding-top:5vw;padding-right:5vw;padding-bottom:5vw;padding-left:5vw;min-height:70vh"
+>
+	<span
+	aria-hidden="true"
+	class="wp-block-cover__background has-off-black-950-background-color has-background-dim-30 has-background-dim"
+	></span>
+	<div class="wp-block-cover__inner-container">
+	<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","verticalAlignment":"top"}} -->
+	<div class="wp-block-group">
+		<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"320px"},"spacing":{"blockGap":"24px"}},"layout":{"type":"default"}} -->
+		<div class="wp-block-group">
+		<!-- wp:paragraph {"style":{"layout":{"selfStretch":"fixed","flexSize":"330px"},"typography":{"fontSize":"17px","fontStyle":"normal","fontWeight":"300","lineHeight":"1.4","textTransform":"none","textDecoration":"none","letterSpacing":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+		<p
+			class="has-white-color has-text-color has-link-color"
+			style="font-size:17px;font-style:normal;font-weight:300;letter-spacing:0px;line-height:1.4;text-decoration:none;text-transform:none"
+		>
+			Let 'em Roll is an album by American organist Big John Patton recorded
+			in 1965 and released on the Blue Note label.
+		</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:buttons -->
+		<div class="wp-block-buttons">
+			<!-- wp:button {"textColor":"white","className":"is-style-button-type-primary-dark","style":{"typography":{"fontSize":"17px","fontStyle":"normal","fontWeight":"700","textDecoration":"none","textTransform":"uppercase","letterSpacing":"0px"},"spacing":{"padding":{"top":"14px","bottom":"14px","left":"36px","right":"36px"}},"border":{"radius":"0px","top":{"radius":"0px","width":"0px","style":"none"},"right":{"radius":"0px","width":"0px","style":"none"},"bottom":{"radius":"0px","width":"0px","style":"none"},"left":{"radius":"0px","width":"0px","style":"none"}}}} -->
+			<div class="wp-block-button is-style-button-type-primary-dark">
+			<a
+				class="wp-block-button__link has-white-color has-text-color has-custom-font-size wp-element-button"
+				style="border-radius:0px;border-top-style:none;border-top-width:0px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px;padding-top:14px;padding-right:36px;padding-bottom:14px;padding-left:36px;font-size:17px;font-style:normal;font-weight:700;letter-spacing:0px;text-decoration:none;text-transform:uppercase"
+				>book jan</a
+			>
+			</div>
+			<!-- /wp:button -->
+		</div>
+		<!-- /wp:buttons -->
+		</div>
+		<!-- /wp:group -->
+
+		<!-- wp:group {"className":"is-style-fade-in","style":{"spacing":{"blockGap":"0px"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"default"}} -->
+		<div class="wp-block-group is-style-fade-in">
+		<!-- wp:paragraph {"align":"right","style":{"typography":{"fontSize":"148px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","lineHeight":"0.8","letterSpacing":"-4px"}},"textColor":"luminous-vivid-orange"} -->
+		<p
+			class="has-text-align-right has-luminous-vivid-orange-color has-text-color"
+			style="font-size:148px;font-style:normal;font-weight:700;letter-spacing:-4px;line-height:0.8;text-transform:uppercase"
+		>
+			master
+		</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph {"align":"right","style":{"typography":{"fontSize":"148px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","lineHeight":"0.8","letterSpacing":"-4px"}},"textColor":"white"} -->
+		<p
+			class="has-text-align-right has-white-color has-text-color"
+			style="font-size:148px;font-style:normal;font-weight:700;letter-spacing:-4px;line-height:0.8;text-transform:uppercase"
+		>
+			of magic
+		</p>
+		<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+	</div>
+</div>
+<!-- /wp:cover -->
---
+++ wp-content/themes/master-of-magic-theme/patterns/video-hero-with-title.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Title: Video Hero With Title
+ * Slug: master-of-magic-theme/video-hero-with-title
+ * Categories: heroes
+ * Description: Video hero with title pattern for Master of Magic Theme.
+ *
+ * @package master-of-magic-theme
+ * @formatter Prettier
+ */
+
+?>
+
+<!-- wp:cover {"dimRatio":30,"overlayColor":"off-black-950","isUserOverlayColor":true,"backgroundType":"video","focalPoint":{"x":0.5,"y":0.5},"minHeight":70,"minHeightUnit":"vh","contentPosition":"center center","isDark":false,"sizeSlug":"full","metadata":{"name":"Video Hero With Title"},"align":"full","layout":{"type":"default"}} -->
+<div class="wp-block-cover alignfull is-light" style="min-height:70vh">
+	<span
+	aria-hidden="true"
+	class="wp-block-cover__background has-off-black-950-background-color has-background-dim-30 has-background-dim"
+	></span>
+	<div class="wp-block-cover__inner-container">
+	<!-- wp:group {"className":"is-style-fade-in","style":{"border":{"radius":{"topLeft":"12px","topRight":"12px","bottomLeft":"12px","bottomRight":"12px"}},"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}},"elements":{"link":{"color":{"text":"var:preset|color|luminous-vivid-amber"}}}},"textColor":"luminous-vivid-amber","layout":{"type":"constrained"}} -->
+	<div
+		class="wp-block-group is-style-fade-in has-luminous-vivid-amber-color has-text-color has-link-color"
+		style="border-top-left-radius:12px;border-top-right-radius:12px;border-bottom-left-radius:12px;border-bottom-right-radius:12px;padding-top:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small)"
+	>
+		<!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"2-extra-large"} -->
+		<p class="has-text-align-center has-2-extra-large-font-size">
+		Master of Magic
+		</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+	</div>
+</div>
+<!-- /wp:cover -->

052e6e3e6fd1afd24913c6270253fd3dd811e074
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2026-01-18 17:15:32 +0100

    update 404 template

--- wp-content/themes/master-of-magic-theme/patterns/template-page-404.php
+++ wp-content/themes/master-of-magic-theme/patterns/template-page-404.php
@@ -13,28 +13,33 @@
  */
 
 ?>
-<!-- wp:group {"style":{"dimensions":{"minHeight":""},"spacing":{"padding":{"top":"100px","bottom":"100px"}}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
-<div class="wp-block-group" style="padding-top:100px;padding-bottom:100px">
-	<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-	<div class="wp-block-group">
-	<!-- wp:heading {"level":1} -->
-	<h1 class="wp-block-heading">
-		<?php esc_html_e( '404 - Page Not Found', 'master-of-magic-theme' ); ?>
-	</h1>
-	<!-- /wp:heading -->
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+	<!-- wp:group {"align":"wide","style":{"dimensions":{"minHeight":""},"spacing":{"padding":{"top":"100px","bottom":"100px"}}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
+	<div class="wp-block-group alignwide" style="padding-top:100px;padding-bottom:100px">
+		<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+		<div class="wp-block-group">
+			<!-- wp:heading {"level":1} -->
+			<h1 class="wp-block-heading">
+				<?php esc_html_e( '404 - Page Not Found', 'master-of-magic-theme' ); ?>
+			</h1>
+			<!-- /wp:heading -->
 
-	<!-- wp:paragraph -->
-	<p>
-		<?php
-		esc_html_e(
-			"The page you are looking for doesn't exist, or it has
-      been moved.",
-			'master-of-magic-theme'
-		);
-		?>
-	</p>
-	<!-- /wp:paragraph -->
+			<!-- wp:paragraph -->
+			<p>
+				<?php
+				esc_html_e(
+					"The page you are looking for doesn't exist, or it has
+				been moved.",
+					'master-of-magic-theme'
+				);
+				?>
+			</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
 	</div>
 	<!-- /wp:group -->
 </div>
-<!-- /wp:group -->

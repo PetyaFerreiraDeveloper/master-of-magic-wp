0924564447da7b145092dcab8e7cc4296f5baff7
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2026-01-18 14:47:41 +0100

    create and add footer to pages

---
+++ wp-content/themes/master-of-magic-theme/patterns/footer.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Title: Footer
+ * Slug: master-of-magic-theme/footer
+ * Categories: footer
+ * Description: Footer for the website.
+ *
+ * @package Master_of_Magic_Theme
+ * @formatter Prettier
+ */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Footer"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"primary-dark","textColor":"white","layout":{"type":"constrained"}} -->
+<div
+	class="wp-block-group alignfull has-white-color has-primary-dark-background-color has-text-color has-background has-link-color"
+	style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"
+>
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|extra-large","left":"var:preset|spacing|medium"}}}} -->
+	<div class="wp-block-columns alignwide">
+	<!-- wp:column {"width":"","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+	<div class="wp-block-column has-white-color has-text-color has-link-color">
+		<!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+		<h3
+		class="wp-block-heading has-white-color has-text-color has-link-color"
+		>
+		Follow me
+		</h3>
+		<!-- /wp:heading -->
+
+		<!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"top":"var:preset|spacing|extra-small"}}}} -->
+		<div
+		class="wp-block-columns is-not-stacked-on-mobile"
+		style="padding-top:var(--wp--preset--spacing--extra-small)"
+		>
+		<!-- wp:column {"width":"60px","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+		<div
+			class="wp-block-column has-white-color has-text-color has-link-color"
+			style="flex-basis:60px"
+		>
+			<!-- wp:master-of-magic-blocks/svg-icon {"mediaId":403,"mediaUrl":"http://localhost/wp-content/uploads/2026/01/facebook.svg","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"textAlign":"center"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"textColor":"white"} /-->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"verticalAlignment":"center","width":"60px"} -->
+		<div
+			class="wp-block-column is-vertically-aligned-center"
+			style="flex-basis:60px"
+		>
+			<!-- wp:master-of-magic-blocks/svg-icon {"mediaId":407,"mediaUrl":"http://localhost/wp-content/uploads/2026/01/instagram.svg","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"textColor":"white"} /-->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"verticalAlignment":"center","width":"60px"} -->
+		<div
+			class="wp-block-column is-vertically-aligned-center"
+			style="flex-basis:60px"
+		>
+			<!-- wp:master-of-magic-blocks/svg-icon {"mediaId":417,"mediaUrl":"http://localhost/wp-content/uploads/2026/01/youtube-4.svg","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} /-->
+		</div>
+		<!-- /wp:column -->
+		</div>
+		<!-- /wp:columns -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column -->
+	<div class="wp-block-column">
+		<!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+		<h3
+		class="wp-block-heading has-white-color has-text-color has-link-color"
+		>
+		Shows
+		</h3>
+		<!-- /wp:heading -->
+
+		<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|extra-small"}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+		<!-- wp:paragraph {"className":"is-style-plain-link"} -->
+		<p class="is-style-plain-link"><a href="/">Konfirmation</a></p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph {"className":"is-style-plain-link"} -->
+		<p class="is-style-plain-link">
+			<a href="/">Underholdning til firmafest</a>
+		</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph {"className":"is-style-plain-link"} -->
+		<p class="is-style-plain-link">
+			<a href="/">Underholdning til julefrokost</a>
+		</p>
+		<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column -->
+	<div class="wp-block-column">
+		<!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+		<h3
+		class="wp-block-heading has-white-color has-text-color has-link-color"
+		>
+		Contact me
+		</h3>
+		<!-- /wp:heading -->
+
+		<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|extra-small"}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+		<!-- wp:paragraph {"className":"is-style-plain-link"} -->
+		<p class="is-style-plain-link">
+			Mail: <a href="mailto:info@tryllejan.dk">info@tryllejan.dk</a>
+		</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph {"className":"is-style-plain-link"} -->
+		<p class="is-style-plain-link">
+			Telefon: <a href="tel:40730193">40 73 01 93</a>
+		</p>
+		<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:paragraph {"align":"center"} -->
+	<p class="has-text-align-center">
+	© Master of Magic&nbsp;2026. Alle rettigheder forbeholdt.
+	</p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
---
+++ wp-content/themes/master-of-magic-theme/patterns/header.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Header
+ * Slug: master-of-magic-theme/header
+ * Categories: header
+ * Description: Header for the website.
+ *
+ * @package Master_of_Magic_Theme
+ * @formatter Prettier
+ */

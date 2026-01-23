b001af3256970b42c6973a26bb979be338fab14d
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2026-01-23 11:01:26 +0100

    event types template

---
+++ wp-content/themes/master-of-magic-theme/patterns/three-column-cards.php
@@ -0,0 +1,262 @@
+<?php
+/**
+ * Title: Three Column Cards
+ * Slug: master-of-magic-theme/three-column-cards
+ * Categories: heroes
+ * Description: Three column cards pattern for Master of Magic Theme.
+ *
+ * @package master-of-magic-theme
+ * @formatter Prettier
+ */
+
+?>
+
+<!-- wp:group {"templateLock":false,"lock":{"move":false,"remove":false},"metadata":{"patternName":"core/block/225","name":"3 products with headline","categories":[]},"align":"wide","className":"is-style-rbc-sm-align\u002d\u002dfull is-style-rbc-md-align\u002d\u002dfull","style":{"spacing":{"padding":{"top":"var:preset|spacing|extra-large","bottom":"var:preset|spacing|extra-large","left":"var:preset|spacing|large","right":"var:preset|spacing|large"},"blockGap":"32px"}},"backgroundColor":"background-light","layout":{"type":"constrained"}} -->
+<div
+	class="wp-block-group alignwide is-style-rbc-sm-align--full is-style-rbc-md-align--full has-background-light-background-color has-background"
+	style="padding-top:var(--wp--preset--spacing--extra-large);padding-right:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--extra-large);padding-left:var(--wp--preset--spacing--large)"
+>
+	<!-- wp:columns {"verticalAlignment":"bottom","align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|large"}}}} -->
+	<div
+	class="wp-block-columns alignwide are-vertically-aligned-bottom"
+	style="padding-top:0;padding-bottom:var(--wp--preset--spacing--large)"
+	>
+	<!-- wp:column {"verticalAlignment":"bottom","width":"100%"} -->
+	<div
+		class="wp-block-column is-vertically-aligned-bottom"
+		style="flex-basis:100%"
+	>
+		<!-- wp:heading {"align":"full","className":"","style":{"elements":{"link":{"color":{"text":"var:preset|color|off-black-950"}}}},"textColor":"off-black-950"} -->
+		<h2
+		class="wp-block-heading alignfull has-off-black-950-color has-text-color has-link-color"
+		>
+		A single product or the full suite.<br />You decide.
+		</h2>
+		<!-- /wp:heading -->
+
+		<!-- wp:heading {"level":3} -->
+		<h3 class="wp-block-heading">
+		The right solution.<br />Built around your people.
+		</h3>
+		<!-- /wp:heading -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column {"verticalAlignment":"bottom","width":"100%"} -->
+	<div
+		class="wp-block-column is-vertically-aligned-bottom"
+		style="flex-basis:100%"
+	>
+		<!-- wp:paragraph {"align":"left","className":"is-style-rbc-lg-text-align\u002d\u002dright","fontSize":"regular"} -->
+		<p
+		class="has-text-align-left is-style-rbc-lg-text-align--right has-regular-font-size"
+		>
+		Whether&nbsp;you need to&nbsp;fix one part of your setup or move&nbsp;on
+		from disconnected systems,&nbsp;you’ll&nbsp;find&nbsp;a solution that
+		meets you where you are.&nbsp;
+		</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|extra-large","left":"var:preset|spacing|large"},"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
+	<div
+	class="wp-block-columns alignwide"
+	style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"
+	>
+	<!-- wp:column {"width":"33.34%"} -->
+	<div class="wp-block-column" style="flex-basis:33.34%">
+		<!-- wp:group {"style":{"spacing":{"blockGap":"0"},"border":{"radius":{"topLeft":"4px","topRight":"4px","bottomLeft":"4px","bottomRight":"4px"}}},"backgroundColor":"primary-dark","layout":{"type":"flex","orientation":"vertical"}} -->
+		<div
+		class="wp-block-group has-primary-dark-background-color has-background"
+		style="border-top-left-radius:4px;border-top-right-radius:4px;border-bottom-left-radius:4px;border-bottom-right-radius:4px"
+		>
+		<!-- wp:image {"id":309,"aspectRatio":"16/9","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":{"topLeft":"4px","topRight":"4px","bottomLeft":"4px","bottomRight":"4px"}}}} -->
+		<figure class="wp-block-image size-full has-custom-border">
+			<img
+			src="http://localhost/wp-content/uploads/2026/01/jan-with-glass-scaled.jpg"
+			alt=""
+			class="wp-image-309"
+			style="border-top-left-radius:4px;border-top-right-radius:4px;border-bottom-left-radius:4px;border-bottom-right-radius:4px;aspect-ratio:16/9;object-fit:cover"
+			/>
+		</figure>
+		<!-- /wp:image -->
+
+		<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|small","left":"var:preset|spacing|small","top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}}},"layout":{"type":"constrained"}} -->
+		<div
+			class="wp-block-group"
+			style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"
+		>
+			<!-- wp:heading {"level":4,"style":{"elements":{"link":{"color":{"text":"var:preset|color|background-light"}}}},"textColor":"background-light"} -->
+			<h4
+			class="wp-block-heading has-background-light-color has-text-color has-link-color"
+			>
+			Firmafest
+			</h4>
+			<!-- /wp:heading -->
+		</div>
+		<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+
+		<!-- wp:paragraph {"fontSize":"regular"} -->
+		<p class="has-regular-font-size">
+		From hiring to offboarding,&nbsp;a flexible HR system that lifts every
+		step of the employee journey.&nbsp;
+		</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:buttons -->
+		<div class="wp-block-buttons">
+		<!-- wp:button {"className":"is-style-button-type-text-link is-style-button-type-primary-dark is-style-button-icon-right is-style-button-icon-mode-inherit is-style-button-size-l","fontSize":"regular","iconUrl":"http://localhost/wp-content/uploads/2025/11/nav-arrow-right.svg"} -->
+		<div
+			class="wp-block-button is-style-button-type-text-link is-style-button-type-primary-dark is-style-button-icon-right is-style-button-icon-mode-inherit is-style-button-size-l"
+			style="--custom-icon-url:url(http://localhost/wp-content/uploads/2025/11/nav-arrow-right.svg)"
+		>
+			<a
+			class="wp-block-button__link has-regular-font-size has-custom-font-size wp-element-button"
+			href="http://localhost/firma-fest/"
+			>Go to overview<img
+				class="wp-image-372"
+				style="width: 24px;"
+				src=""
+				alt=""
+			/></a>
+		</div>
+		<!-- /wp:button -->
+		</div>
+		<!-- /wp:buttons -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column {"width":"33.34%"} -->
+	<div class="wp-block-column" style="flex-basis:33.34%">
+		<!-- wp:group {"style":{"spacing":{"blockGap":"0"},"border":{"radius":{"topLeft":"4px","topRight":"4px","bottomLeft":"4px","bottomRight":"4px"}}},"backgroundColor":"primary-magic","layout":{"type":"flex","orientation":"vertical"}} -->
+		<div
+		class="wp-block-group has-primary-magic-background-color has-background"
+		style="border-top-left-radius:4px;border-top-right-radius:4px;border-bottom-left-radius:4px;border-bottom-right-radius:4px"
+		>
+		<!-- wp:image {"id":90,"aspectRatio":"16/9","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":{"topLeft":"4px","topRight":"4px","bottomLeft":"4px","bottomRight":"4px"}}}} -->
+		<figure class="wp-block-image size-full has-custom-border">
+			<img
+			src="http://localhost/wp-content/uploads/2025/11/TrylleJan-2-1024x432-1.jpg"
+			alt=""
+			class="wp-image-90"
+			style="border-top-left-radius:4px;border-top-right-radius:4px;border-bottom-left-radius:4px;border-bottom-right-radius:4px;aspect-ratio:16/9;object-fit:cover"
+			/>
+		</figure>
+		<!-- /wp:image -->
+
+		<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|small","left":"var:preset|spacing|small","top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}}},"layout":{"type":"constrained"}} -->
+		<div
+			class="wp-block-group"
+			style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"
+		>
+			<!-- wp:heading {"level":4} -->
+			<h4 class="wp-block-heading">Julefrokost</h4>
+			<!-- /wp:heading -->
+		</div>
+		<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+
+		<!-- wp:paragraph {"fontSize":"regular"} -->
+		<p class="has-regular-font-size">
+		From hiring to offboarding,&nbsp;a flexible HR system that lifts every
+		step of the employee journey.&nbsp;
+		</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:buttons -->
+		<div class="wp-block-buttons">
+		<!-- wp:button {"backgroundColor":"primary-magic","className":"is-style-button-type-text-link is-style-button-type-primary-dark is-style-button-icon-right is-style-button-icon-mode-inherit is-style-button-size-l","fontSize":"regular","iconUrl":"http://localhost/wp-content/uploads/2025/11/nav-arrow-right.svg"} -->
+		<div
+			class="wp-block-button is-style-button-type-text-link is-style-button-type-primary-dark is-style-button-icon-right is-style-button-icon-mode-inherit is-style-button-size-l"
+			style="--custom-icon-url:url(http://localhost/wp-content/uploads/2025/11/nav-arrow-right.svg)"
+		>
+			<a
+			class="wp-block-button__link has-primary-magic-background-color has-background has-regular-font-size has-custom-font-size wp-element-button"
+			href="/julefrokost"
+			>Go to overview<img
+				class="wp-image-372"
+				style="width: 24px;"
+				src=""
+				alt=""
+			/></a>
+		</div>
+		<!-- /wp:button -->
+		</div>
+		<!-- /wp:buttons -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column {"width":"33.34%"} -->
+	<div class="wp-block-column" style="flex-basis:33.34%">
+		<!-- wp:group {"style":{"spacing":{"blockGap":"0"},"border":{"radius":{"topLeft":"4px","topRight":"4px","bottomLeft":"4px","bottomRight":"4px"}}},"backgroundColor":"secondary-magic","layout":{"type":"flex","orientation":"vertical"}} -->
+		<div
+		class="wp-block-group has-secondary-magic-background-color has-background"
+		style="border-top-left-radius:4px;border-top-right-radius:4px;border-bottom-left-radius:4px;border-bottom-right-radius:4px"
+		>
+		<!-- wp:image {"id":215,"aspectRatio":"16/9","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":{"topLeft":"4px","topRight":"4px","bottomLeft":"4px","bottomRight":"4px"}}}} -->
+		<figure class="wp-block-image size-full has-custom-border">
+			<img
+			src="http://localhost/wp-content/uploads/2026/01/milad-fakurian-iFHGJUgFymw-unsplash-scaled.jpg"
+			alt=""
+			class="wp-image-215"
+			style="border-top-left-radius:4px;border-top-right-radius:4px;border-bottom-left-radius:4px;border-bottom-right-radius:4px;aspect-ratio:16/9;object-fit:cover"
+			/>
+		</figure>
+		<!-- /wp:image -->
+
+		<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|small","left":"var:preset|spacing|small","top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}}},"layout":{"type":"constrained"}} -->
+		<div
+			class="wp-block-group"
+			style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"
+		>
+			<!-- wp:heading {"level":4,"style":{"elements":{"link":{"color":{"text":"var:preset|color|background-light"}}}},"textColor":"background-light"} -->
+			<h4
+			class="wp-block-heading has-background-light-color has-text-color has-link-color"
+			>
+			Konfirmation
+			</h4>
+			<!-- /wp:heading -->
+		</div>
+		<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+
+		<!-- wp:paragraph {"fontSize":"regular"} -->
+		<p class="has-regular-font-size">
+		From hiring to offboarding,&nbsp;a flexible HR system that lifts every
+		step of the employee journey.&nbsp;
+		</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:buttons -->
+		<div class="wp-block-buttons">
+		<!-- wp:button {"backgroundColor":"secondary-magic","className":"is-style-button-type-text-link is-style-button-type-primary-dark is-style-button-icon-right is-style-button-icon-mode-inherit is-style-button-size-l","fontSize":"regular","iconUrl":"http://localhost/wp-content/uploads/2025/11/nav-arrow-right.svg"} -->
+		<div
+			class="wp-block-button is-style-button-type-text-link is-style-button-type-primary-dark is-style-button-icon-right is-style-button-icon-mode-inherit is-style-button-size-l"
+			style="--custom-icon-url:url(http://localhost/wp-content/uploads/2025/11/nav-arrow-right.svg)"
+		>
+			<a
+			class="wp-block-button__link has-secondary-magic-background-color has-background has-regular-font-size has-custom-font-size wp-element-button"
+			href="/konfirmation"
+			>Go to overview<img
+				class="wp-image-372"
+				style="width: 24px;"
+				src=""
+				alt=""
+			/></a>
+		</div>
+		<!-- /wp:button -->
+		</div>
+		<!-- /wp:buttons -->
+	</div>
+	<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+</div>
+<!-- /wp:group -->

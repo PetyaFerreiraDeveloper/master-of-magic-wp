da441509ed89d1b9e32bd37b734f5ed320cfee47
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2026-01-23 16:15:58 +0100

    add references pattern

---
+++ wp-content/themes/master-of-magic-theme/patterns/references.php
@@ -0,0 +1,195 @@
+<?php
+/**
+ * Title: References Slider
+ * Slug: master-of-magic-theme/references-slider
+ * Categories: heroes
+ * Description: References slider pattern for Master of Magic Theme.
+ *
+ * @package master-of-magic-theme
+ * @formatter Prettier
+ */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"References"},"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide">
+	<!-- wp:columns {"verticalAlignment":"bottom","align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|large"}}}} -->
+	<div
+	class="wp-block-columns alignwide are-vertically-aligned-bottom"
+	style="padding-top:0;padding-bottom:var(--wp--preset--spacing--large)"
+	>
+	<!-- wp:column {"verticalAlignment":"bottom","width":"100%"} -->
+	<div
+		class="wp-block-column is-vertically-aligned-bottom"
+		style="flex-basis:100%"
+	>
+		<!-- wp:paragraph {"align":"left","className":"is-style-rbc-lg-text-align\u002d\u002dright","fontSize":"regular"} -->
+		<p
+		class="has-text-align-left is-style-rbc-lg-text-align--right has-regular-font-size"
+		>
+		Udtalelser fra firmafester, julefrokoster og større arrangementer.
+		</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column {"verticalAlignment":"bottom","width":"100%"} -->
+	<div
+		class="wp-block-column is-vertically-aligned-bottom"
+		style="flex-basis:100%"
+	>
+		<!-- wp:heading {"align":"full","className":"","style":{"elements":{"link":{"color":{"text":"var:preset|color|off-black-950"}}}},"textColor":"off-black-950"} -->
+		<h2
+		class="wp-block-heading alignfull has-off-black-950-color has-text-color has-link-color"
+		>
+		Hvad siger kunderne?
+		</h2>
+		<!-- /wp:heading -->
+
+		<!-- wp:heading {"level":3} -->
+		<h3 class="wp-block-heading">
+		Brugt af virksomheder og foreninger i hele landet
+		</h3>
+		<!-- /wp:heading -->
+	</div>
+	<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:master-of-magic-blocks/slider {"align":"wide"} -->
+	<!-- wp:master-of-magic-blocks/slider-slide -->
+	<div class="wp-block-master-of-magic-blocks-slider-slide swiper-slide">
+	<!-- wp:group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+		<!-- wp:image {"id":90,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+		<figure class="wp-block-image size-full">
+		<img
+			src="http://localhost/wp-content/uploads/2025/11/TrylleJan-2-1024x432-1.jpg"
+			alt=""
+			class="wp-image-90"
+			style="aspect-ratio:1;object-fit:cover"
+		/>
+		</figure>
+		<!-- /wp:image -->
+
+		<!-- wp:post-date {"datetime":"2018-03-12T12:35:00","textAlign":"right","style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"var:preset|spacing|extra-small","bottom":"0"}}}} /-->
+
+		<!-- wp:paragraph -->
+		<p>
+		Fantastisk oplevelse at have TrylleJan på besøg til vores arrangement i
+		Fagforeningen. Han tryllebandt alle mennesker i teltet, store som små.
+		Eneste minus var at de 40 min gik for hurtigt. TrylleJan får de varmeste
+		anbefalinger med, og kunne jeg give 10 stjerne så skulle han have det.
+		</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:heading {"level":5} -->
+		<h5 class="wp-block-heading">3F Roskilde</h5>
+		<!-- /wp:heading -->
+	</div>
+	<!-- /wp:group -->
+	</div>
+	<!-- /wp:master-of-magic-blocks/slider-slide -->
+
+	<!-- wp:master-of-magic-blocks/slider-slide -->
+	<div class="wp-block-master-of-magic-blocks-slider-slide swiper-slide">
+	<!-- wp:group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+		<!-- wp:image {"id":9,"aspectRatio":"1","scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
+		<figure class="wp-block-image size-large">
+		<img
+			src="http://localhost/wp-content/uploads/2025/10/40-scaled-1-683x1024.jpg"
+			alt=""
+			class="wp-image-9"
+			style="aspect-ratio:1;object-fit:cover"
+		/>
+		</figure>
+		<!-- /wp:image -->
+
+		<!-- wp:post-date {"datetime":"2018-03-12T12:35:00","textAlign":"right","style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"var:preset|spacing|extra-small","bottom":"0"}}}} /-->
+
+		<!-- wp:paragraph -->
+		<p>
+		Der skal lyde en stor tak til TrylleJan for en god optræden til vores
+		juletræsfest d. 10/12 2017 i Dansk El-Forbund København.
+		</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:heading {"level":5} -->
+		<h5 class="wp-block-heading">Dansk El-Forbund afd. København</h5>
+		<!-- /wp:heading -->
+	</div>
+	<!-- /wp:group -->
+	</div>
+	<!-- /wp:master-of-magic-blocks/slider-slide -->
+
+	<!-- wp:master-of-magic-blocks/slider-slide -->
+	<div class="wp-block-master-of-magic-blocks-slider-slide swiper-slide">
+	<!-- wp:group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+		<!-- wp:image {"id":309,"aspectRatio":"1","scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
+		<figure class="wp-block-image size-large">
+		<img
+			src="http://localhost/wp-content/uploads/2026/01/jan-with-glass-1024x1024.jpg"
+			alt=""
+			class="wp-image-309"
+			style="aspect-ratio:1;object-fit:cover"
+		/>
+		</figure>
+		<!-- /wp:image -->
+
+		<!-- wp:post-date {"datetime":"2018-03-12T12:35:00","textAlign":"right","style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"var:preset|spacing|extra-small","bottom":"0"}}}} /-->
+
+		<!-- wp:paragraph -->
+		<p>
+		Kan på det varmeste anbefale Tryllejan og hans hjælper Magiske Emil (Der
+		er ferm til at lave ballondyr, han lavede en elg og gjorde en dame meget
+		meget glad :-)). TrylleJan elsker helt tydeligt sit fag, har en skøn
+		timing og gav voksne og børn fest. Tak for det.
+		</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:heading {"level":5} -->
+		<h5 class="wp-block-heading">Kolonihaveforeningen</h5>
+		<!-- /wp:heading -->
+	</div>
+	<!-- /wp:group -->
+	</div>
+	<!-- /wp:master-of-magic-blocks/slider-slide -->
+
+	<!-- wp:master-of-magic-blocks/slider-slide -->
+	<div class="wp-block-master-of-magic-blocks-slider-slide swiper-slide">
+	<!-- wp:group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+		<!-- wp:image {"id":88,"aspectRatio":"1","scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
+		<figure class="wp-block-image size-large">
+		<img
+			src="http://localhost/wp-content/uploads/2025/11/Jan-uden-krop-scaled-e1769161782542-960x1024.jpg"
+			alt=""
+			class="wp-image-88"
+			style="aspect-ratio:1;object-fit:cover"
+		/>
+		</figure>
+		<!-- /wp:image -->
+
+		<!-- wp:post-date {"datetime":"2018-03-12T12:35:00","textAlign":"right","style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"var:preset|spacing|extra-small","bottom":"0"}}}} /-->
+
+		<!-- wp:paragraph -->
+		<p>
+		Skøn afslutning på et dejligt håndboldstævne. TrylleJan lavede nogle
+		fantastiske tryllekunstner, og børnene elskede det. Der var stor iver
+		efter at komme op på scenen og endnu større undren over, hvordan det dog
+		kunne lade sig gøre at trylle.
+		</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:heading {"level":5} -->
+		<h5 class="wp-block-heading">Håndbold-stævne</h5>
+		<!-- /wp:heading -->
+	</div>
+	<!-- /wp:group -->
+	</div>
+	<!-- /wp:master-of-magic-blocks/slider-slide -->
+	<!-- /wp:master-of-magic-blocks/slider -->
+</div>
+<!-- /wp:group -->

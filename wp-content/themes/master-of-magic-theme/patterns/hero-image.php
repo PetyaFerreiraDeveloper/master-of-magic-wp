97958b44db1e7889a9cbabcd474fad671a0af04f
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2026-01-24 13:16:29 +0100

    update hero image

---
+++ wp-content/themes/master-of-magic-theme/patterns/hero-image.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Title: Hero With Image and Text
+ * Slug: master-of-magic-theme/hero-with-image-and-text
+ * Categories: heroes
+ * Description: Image hero pattern for Master of Magic Theme.
+ *
+ * @package master-of-magic-theme
+ * @formatter Prettier
+ */
+
+?>
+
+<!-- wp:cover {
+"dimRatio":30,"overlayColor":"primary-dark","isUserOverlayColor":true,"minHeight":70,"minHeightUnit":"vh","sizeSlug":"large","metadata":{"name":"Hero With Image and Text","categories":["heroes"],"patternName":"master-of-magic-theme/hero-with-image-and-text"},"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull" style="min-height:70vh">
+	<span
+	aria-hidden="true"
+	class="wp-block-cover__background has-primary-dark-background-color has-background-dim-30 has-background-dim"
+	></span>
+	<div class="wp-block-cover__inner-container">
+	<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignwide">
+		<!-- wp:columns {"align":"wide","className":"is-style-rbc-md-columns-direction\u002d\u002dreverse is-style-rbc-sm-columns-direction\u002d\u002dreverse","style":{"spacing":{"blockGap":{"top":"100px"}}}} -->
+		<div
+		class="wp-block-columns alignwide is-style-rbc-md-columns-direction--reverse is-style-rbc-sm-columns-direction--reverse"
+		>
+		<!-- wp:column {"width":"33%"} -->
+		<div class="wp-block-column" style="flex-basis:33%">
+			<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"320px"},"spacing":{"blockGap":"24px"}},"layout":{"type":"default"}} -->
+			<div class="wp-block-group">
+			<!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fixed","flexSize":"330px"},"typography":{"fontSize":"17px","fontStyle":"normal","fontWeight":"300","lineHeight":"1.4","textTransform":"none","textDecoration":"none","letterSpacing":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+			<p
+				class="has-text-align-center has-white-color has-text-color has-link-color"
+				style="font-size:17px;font-style:normal;font-weight:300;letter-spacing:0px;line-height:1.4;text-decoration:none;text-transform:none"
+			>
+				Underholdning, der samler både konfirmanden, vennerne og de voksne
+				–<br />med grin, overraskelse og fælles oplevelser.
+			</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+			<div class="wp-block-buttons">
+				<!-- wp:button {"className":"is-style-button-type-cta-gold is-style-button-size-m","style":{"typography":{"textTransform":"capitalize"}},"fontSize":"medium"} -->
+				<div
+				class="wp-block-button is-style-button-type-cta-gold is-style-button-size-m"
+				>
+				<a
+					class="wp-block-button__link has-medium-font-size has-custom-font-size wp-element-button"
+					style="text-transform:capitalize"
+					><strong>Book underholdning til konfirmation</strong></a
+				>
+				</div>
+				<!-- /wp:button -->
+			</div>
+			<!-- /wp:buttons -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"verticalAlignment":"top","width":"66%"} -->
+		<div
+			class="wp-block-column is-vertically-aligned-top"
+			style="flex-basis:66%"
+		>
+			<!-- wp:group {"className":"is-style-fade-in","style":{"spacing":{"blockGap":"0px"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"default"}} -->
+			<div class="wp-block-group is-style-fade-in">
+			<!-- wp:paragraph {"align":"right","style":{"typography":{"fontSize":"74px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","lineHeight":"0.8","letterSpacing":"-4px"},"elements":{"link":{"color":{"text":"var:preset|color|primary-magic"}}}},"textColor":"primary-magic"} -->
+			<p
+				class="has-text-align-right has-primary-magic-color has-text-color has-link-color"
+				style="font-size:74px;font-style:normal;font-weight:700;letter-spacing:-4px;line-height:0.8;text-transform:uppercase"
+			>
+				Trylleshow
+			</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"align":"right","style":{"typography":{"fontSize":"74px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","lineHeight":"0.8","letterSpacing":"-4px"}},"textColor":"white"} -->
+			<p
+				class="has-text-align-right has-white-color has-text-color"
+				style="font-size:74px;font-style:normal;font-weight:700;letter-spacing:-4px;line-height:0.8;text-transform:uppercase"
+			>
+				<strong>til konfirmation</strong><br />
+			</p>
+			<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+		</div>
+		<!-- /wp:columns -->
+	</div>
+	<!-- /wp:group -->
+	</div>
+</div>
+<!-- /wp:cover -->

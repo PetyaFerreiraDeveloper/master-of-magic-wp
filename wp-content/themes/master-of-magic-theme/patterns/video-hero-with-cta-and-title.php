ae32b3975e744c8d27cbc9012eb3e1a68efb6ece
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2026-01-24 11:02:56 +0100

    add a group to limit hero content width

--- wp-content/themes/master-of-magic-theme/patterns/video-hero-with-cta-and-title.php
+++ wp-content/themes/master-of-magic-theme/patterns/video-hero-with-cta-and-title.php
@@ -16,79 +16,82 @@
 	class="wp-block-cover alignfull is-light"
 	style="margin-top:0;padding-top:5vw;padding-right:5vw;padding-bottom:5vw;padding-left:5vw;min-height:70vh"
 >
-</video
-	><span
+	<span
 	aria-hidden="true"
 	class="wp-block-cover__background has-off-black-950-background-color has-background-dim"
 	></span>
 	<div class="wp-block-cover__inner-container">
-	<!-- wp:columns {"className":"is-style-rbc-md-columns-direction\u002d\u002dreverse is-style-rbc-sm-columns-direction\u002d\u002dreverse","style":{"spacing":{"blockGap":{"top":"100px"}}}} -->
-	<div
-		class="wp-block-columns is-style-rbc-md-columns-direction--reverse is-style-rbc-sm-columns-direction--reverse"
-	>
+	<!-- wp:group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+		<!-- wp:columns {"align":"wide","className":"is-style-rbc-md-columns-direction\u002d\u002dreverse is-style-rbc-sm-columns-direction\u002d\u002dreverse","style":{"spacing":{"blockGap":{"top":"100px"}}}} -->
+		<div
+		class="wp-block-columns alignwide is-style-rbc-md-columns-direction--reverse is-style-rbc-sm-columns-direction--reverse"
+		>
 		<!-- wp:column {"width":"33%"} -->
 		<div class="wp-block-column" style="flex-basis:33%">
-		<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"320px"},"spacing":{"blockGap":"24px"}},"layout":{"type":"default"}} -->
-		<div class="wp-block-group">
+			<!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"320px"},"spacing":{"blockGap":"24px"}},"layout":{"type":"default"}} -->
+			<div class="wp-block-group">
 			<!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fixed","flexSize":"330px"},"typography":{"fontSize":"17px","fontStyle":"normal","fontWeight":"300","lineHeight":"1.4","textTransform":"none","textDecoration":"none","letterSpacing":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
 			<p
-			class="has-text-align-center has-white-color has-text-color has-link-color"
-			style="font-size:17px;font-style:normal;font-weight:300;letter-spacing:0px;line-height:1.4;text-decoration:none;text-transform:none"
+				class="has-text-align-center has-white-color has-text-color has-link-color"
+				style="font-size:17px;font-style:normal;font-weight:300;letter-spacing:0px;line-height:1.4;text-decoration:none;text-transform:none"
 			>
-			Underholdning, der samler gæsterne og skaber fælles oplevelser.
+				Underholdning, der samler gæsterne og skaber fælles oplevelser.
 			</p>
 			<!-- /wp:paragraph -->
 
 			<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
 			<div class="wp-block-buttons">
-			<!-- wp:button {"className":"is-style-button-size-m is-style-button-type-cta-gold","style":{"typography":{"textTransform":"uppercase"}},"fontSize":"medium"} -->
-			<div
+				<!-- wp:button {"className":"is-style-button-size-m is-style-button-type-cta-gold","style":{"typography":{"textTransform":"uppercase"}},"fontSize":"medium"} -->
+				<div
 				class="wp-block-button is-style-button-size-m is-style-button-type-cta-gold"
-			>
-				<a
-				class="wp-block-button__link has-medium-font-size has-custom-font-size wp-element-button"
-				style="text-transform:uppercase"
-				>book underholding</a
 				>
-			</div>
-			<!-- /wp:button -->
+				<a
+					class="wp-block-button__link has-medium-font-size has-custom-font-size wp-element-button"
+					style="text-transform:uppercase"
+					>book underholding</a
+				>
+				</div>
+				<!-- /wp:button -->
 			</div>
 			<!-- /wp:buttons -->
-		</div>
-		<!-- /wp:group -->
+			</div>
+			<!-- /wp:group -->
 		</div>
 		<!-- /wp:column -->
 
 		<!-- wp:column {"verticalAlignment":"top","width":"66%"} -->
 		<div
-		class="wp-block-column is-vertically-aligned-top"
-		style="flex-basis:66%"
+			class="wp-block-column is-vertically-aligned-top"
+			style="flex-basis:66%"
 		>
-		<!-- wp:group {"className":"is-style-fade-in","style":{"spacing":{"blockGap":"0px"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"default"}} -->
-		<div class="wp-block-group is-style-fade-in">
+			<!-- wp:group {"className":"is-style-fade-in","style":{"spacing":{"blockGap":"0px"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"default"}} -->
+			<div class="wp-block-group is-style-fade-in">
 			<!-- wp:paragraph {"align":"right","style":{"typography":{"fontSize":"148px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","lineHeight":"0.8","letterSpacing":"-4px"},"elements":{"link":{"color":{"text":"var:preset|color|primary-magic"}}}},"textColor":"primary-magic"} -->
 			<p
-			class="has-text-align-right has-primary-magic-color has-text-color has-link-color"
-			style="font-size:148px;font-style:normal;font-weight:700;letter-spacing:-4px;line-height:0.8;text-transform:uppercase"
+				class="has-text-align-right has-primary-magic-color has-text-color has-link-color"
+				style="font-size:148px;font-style:normal;font-weight:700;letter-spacing:-4px;line-height:0.8;text-transform:uppercase"
 			>
-			master
+				master
 			</p>
 			<!-- /wp:paragraph -->
 
 			<!-- wp:paragraph {"align":"right","style":{"typography":{"fontSize":"148px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","lineHeight":"0.8","letterSpacing":"-4px"}},"textColor":"white"} -->
 			<p
-			class="has-text-align-right has-white-color has-text-color"
-			style="font-size:148px;font-style:normal;font-weight:700;letter-spacing:-4px;line-height:0.8;text-transform:uppercase"
+				class="has-text-align-right has-white-color has-text-color"
+				style="font-size:148px;font-style:normal;font-weight:700;letter-spacing:-4px;line-height:0.8;text-transform:uppercase"
 			>
-			of magic
+				of magic
 			</p>
 			<!-- /wp:paragraph -->
-		</div>
-		<!-- /wp:group -->
+			</div>
+			<!-- /wp:group -->
 		</div>
 		<!-- /wp:column -->
+		</div>
+		<!-- /wp:columns -->
 	</div>
-	<!-- /wp:columns -->
+	<!-- /wp:group -->
 	</div>
 </div>
 <!-- /wp:cover -->
